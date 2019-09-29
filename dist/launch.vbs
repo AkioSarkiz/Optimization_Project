36610fa850ea7fa750033efb80c97a9bdafb3e0b
AkioSarkiz <akiosarkiz@gmail.com>
2019-09-29 03:38:38 +0300

    delete netbeans config files

--- dist/launch.vbs
+++
@@ -1,2 +0,0 @@
-set sh=CreateObject("Wscript.Shell")
-sh.Run """run_app.cmd"" 1", 0
--- dist/run_app.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-(echo.set sh=CreateObject^("Wscript.Shell"^)
-echo.sh.Run """%~nx0"" 1", 0)>launch.vbs
-if "%~1"=="" (start "" "launch.vbs"&exit /b)
-java -jar Optimization_project.jar
